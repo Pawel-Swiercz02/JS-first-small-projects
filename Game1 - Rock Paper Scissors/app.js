1711068fef4b09a84bba2aa00f84d7b812698759
Paweł Świercz <150139537+Pawel-Swiercz02@users.noreply.github.com>
2023-12-19 16:04:09 +0100

    Add files via upload

---
+++ Game1 - Rock Paper Scissors/app.js
@@ -0,0 +1,58 @@
+const computerChoiceSpan = document.getElementById('computer-choice');
+const yourChoiceSpan = document.getElementById('your-choice');
+const resultSpan = document.getElementById('result');
+
+//Pick out all the buttons
+const possibleChoices = document.querySelectorAll('button');
+let yourChoice;
+let computerChoice;
+let finalResult;
+
+                //For each possibleChoice add event listener to listen up for a click (if I click any of the buttons I want something to happen)
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    yourChoice = e.target.id;
+    yourChoiceSpan.innerHTML = yourChoice;
+    generateComputerChoice()
+    getResult();
+}));
+
+function generateComputerChoice() {
+    //Random number (math floor makes it a full integer) - it would generate a number form 0 to 2, so we add +1 for readability - now its 1 to 3
+    const randomNum = Math.floor(Math.random() * 3) + 1 ; //Or you can use possibleChoices.length
+
+    if (randomNum === 1) {
+        computerChoice = 'rock';
+    }
+    if (randomNum === 2) {
+        computerChoice = 'paper';
+    }
+    if (randomNum === 3) {
+        computerChoice = 'scissors';
+    }
+    computerChoiceSpan.innerHTML = computerChoice;
+}
+
+function getResult() {
+    if (computerChoice === yourChoice) {
+        finalResult = "It's a draw!";
+    }
+    if (computerChoice === 'rock' && yourChoice === 'paper') {
+        finalResult = "You won!";
+    }
+    if (computerChoice === 'rock' && yourChoice === 'scissors') {
+        finalResult = "You lost...";
+    }
+    if (computerChoice === 'paper' && yourChoice === 'rock') {
+        finalResult = "You lost...";
+    }
+    if (computerChoice === 'paper' && yourChoice === 'scissors') {
+        finalResult = "You won!";
+    }
+    if (computerChoice === 'scissors' && yourChoice === 'rock') {
+        finalResult = "You won!";
+    }
+    if (computerChoice === 'scissors' && yourChoice === 'paper') {
+        finalResult = "You lost...";
+    }
+    resultSpan.innerHTML = finalResult;
+}
